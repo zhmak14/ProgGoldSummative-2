0ae71c9e5eaf77a09f6394c12ae0ae77ec3ce913
Alex Zotov <sqck@durham.ac.uk>
2024-04-09 20:14:22 +0100

    city by continent + get cities button (uncombined)

--- client/switch.js
+++ client/switch.js
@@ -1,1 +1,6 @@
-console.log("hello World");
+window.addEventListener('click', function (event) {
+    fetch('http://127.0.0.1:8090/cities')
+        .then(response => response.text())
+        .then(body => document.getElementById('result').innerHTML=body)
+        .catch( (error) => alert(error))
+});
--- server.js
+++ server.js
@@ -1,11 +1,48 @@
-const express = require('express')
+const express = require('express');
+const res = require('express/lib/response');
 const app = express()
+
+const cities = [
+  {"name": "Budapest",
+  "country": "Hungary",
+  "continent": "Europe"},
+  {"name": "Tokyo",
+  "country": "Japan",
+  "continent": "Asia"},
+]
 
 app.use(express.static('client'));
 
-app.get('/', function(req, resp){
-    resp.send('Goodbye World')
+app.get('/cities', function(req, resp){
+    resp.send(cities)
   })
+
+app.get('/city/:name', function(req, resp){
+    let cityName = req.params.name;
+    let found = false;
+    for (let city of cities) {
+        if (city.name.toLowerCase() == cityName.toLowerCase()){
+            resp.send(city);
+            found = true;
+            break;
+        }
+    }
+    if(!found){
+        resp.send("No such city");
+    }
+});
+
+app.get('/citysearch', function(req, resp){
+  let continent = req.query.continent;
+  let results = [];
+  for (let city of cities) {
+      if (city.continent.toLowerCase() == continent.toLowerCase()){
+          results.push(city);
+      }
+  }
+  resp.send(results);
+});
+
 
 app.listen(8090)
 console.log('Server running at http://127.0.0.1:8090/');
