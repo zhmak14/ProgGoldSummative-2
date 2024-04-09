e027e744886794be4966441fb4873580714974b2
Alex Zotov <sqck@durham.ac.uk>
2024-04-09 20:45:26 +0100

    fixed the button so ot works

--- client/switch.js
+++ client/switch.js
@@ -1,4 +1,6 @@
-window.addEventListener('click', function (event) {
+const allButton = document.getElementById("getCities")
+
+allButton.addEventListener('click', function (event) {
     fetch('http://127.0.0.1:8090/cities')
         .then(response => response.text())
         .then(body => document.getElementById('result').innerHTML=body)
